70f0eed7fbd2da1768a1e98ab998fe8853314d6c
Diego Ramirez <diegojramirezs7@gmail.com>
2020-06-02 13:36:47 -0700

    update

---
+++ build/precache-manifest.715805984d8ab3c7d81e7bd8308b6132.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b045813eb934e1e5cc731cce70aaeeb2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0fea5ad00302fafc0e56",
+    "url": "/static/css/main.9f35c52d.chunk.css"
+  },
+  {
+    "revision": "4b38328f02d2cad41e47",
+    "url": "/static/js/2.ba18af5e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba18af5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fea5ad00302fafc0e56",
+    "url": "/static/js/main.daa102d4.chunk.js"
+  },
+  {
+    "revision": "e5cd336dad92a97a8095",
+    "url": "/static/js/runtime-main.ad13e283.js"
+  }
+]);
